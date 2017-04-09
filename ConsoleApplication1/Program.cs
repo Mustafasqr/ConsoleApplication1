8594ef599d5a23202a9c7cec9ef021d0c2e90c68
musta <musta@DESKTOP-LVEVIAB>
2017-04-09 23:44:13 +0300

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //لجعل الخلفية زرقاء
+            Console.BackgroundColor = ConsoleColor.Black;
+            //لتغيير اللون الى الاحمر 
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("mustafa sakr");
+            Console.ReadKey();
+
+        }
+    }
+}
